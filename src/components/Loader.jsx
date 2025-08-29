8b3ad42d51b1dacf46e4f5cc4451c4a6af8ba601
laxman-goud <codewithlaxman92@gmail.com>
2025-08-29 21:34:56 +0530

    feat: add hero section

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import { Html, useProgress } from "@react-three/drei"
 
 const Loader = () => {
+  const { progress } = useProgress()
   return (
-    <div>Loader</div>
+    <Html>
+      <span className="canvas-load"></span>
+      <p className="text-[#f1f1f1] font-bold text-[14px] mt-40">{progress.toFixed(2)}%</p>
+    </Html>
   )
 }
 
