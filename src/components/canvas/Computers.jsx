28477b4d29a2a1ef4bebb759d9332aff56e572b9
laxman-goud <codewithlaxman92@gmail.com>
2025-08-30 17:38:35 +0530

    fix: add ambientLight to ComputersCanvas

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,11 +5,12 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+  const computer = useGLTF("/desktop_pc/scene.gltf");
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
+      <ambientLight intensity={0.87} />
+      <directionalLight position={[5, 5, 5]} intensity={2.7} />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
