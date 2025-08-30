efe7a38d8dd30f0e49049a21df553224cc116d7e
laxman-goud <codewithlaxman92@gmail.com>
2025-08-30 17:55:31 +0530

    docs: update hero, about sections

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ import { fadeIn, textVariant } from '../utils/motion';
 import { services } from '../constants';
 import { SectionWrapper } from '../hoc';
 
-const ServiceCard = ({index, title, icon}) => (
+const ServiceCard = ({ index, title, icon }) => (
   <Tilit className="xs:w-[250px] w-full">
     <motion.div
       variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
@@ -17,12 +17,12 @@ const ServiceCard = ({index, title, icon}) => (
         scale: 1,
         speed: 450
       }} className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
-        <img src={icon} alt={title} className="w-16 h-16 object-contain"/>
+        <img src={icon} alt={title} className="w-16 h-16 object-contain" />
         <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
       </div>
     </motion.div>
   </Tilit>
-) 
+)
 
 
 const About = () => {
@@ -32,9 +32,12 @@ const About = () => {
         <p className={styles.sectionSubText}>Introduction</p>
         <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
-      
+
       <motion.p variants={fadeIn("", "", 0.1, 1)} className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
-        I'm a skilled software developer with experience in TypeScript and JavaScript, and expertise in frameworks like React, Node.js, and Three.js. I'm a quick learner and collaborate closely with clients to create efficient, scalable, and user-friendly solutions that solve real-world problems. Let's work together to bring your ideas to life!
+        I’m an engineering student 🧑‍💻 passionate about web development and creating real-world applications.
+        Currently exploring the MERN stack, APIs, and deployment while leveling up in JavaScript, React, and Node.js.
+        I enjoy building personal projects 💡 that solve small but meaningful problems with modern tools and frameworks.
+        My tech stack includes JavaScript, Python, HTML, CSS, React, Node.js, Express, Next.js, Tailwind CSS, and MongoDB.
       </motion.p>
 
       <div className="mt-20 flex flex-wrap gap-10">
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,8 +19,8 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Laxman</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
+            I develop user interfaces, web <br className='sm:block hidden' />
+            applications and 3D visuals
           </p>
         </div>
       </div>
