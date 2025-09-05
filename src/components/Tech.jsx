15e455cd55e2cf0481ba53ce1cbb154e721bc9a9
laxman-goud <codewithlaxman92@gmail.com>
2025-09-05 14:26:04 +0530

    refactor: comment out experience section in app

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,15 +1,24 @@
 import { BallCanvas } from "./canvas"
 import { SectionWrapper } from "../hoc"
 import { technologies } from "../constants"
+import { motion } from 'framer-motion';
+import { textVariant } from '../utils/motion';
+import { styles } from "../styles"
 
 const Tech = () => {
   return (
-    <div className="flex flex-row flex-wrap justify-center gap-10">
-      {technologies.map((technology) => (
-        <div className="w-28 h-28" key={technology.name}>
-          <BallCanvas icons={[technology.icon]} />
-        </div>
-      ))}
+    <div className="skills flex flex-col gap-8">
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Technologies I work with</p>
+        <h2 className={styles.sectionHeadText}>Skills & Tools.</h2>
+      </motion.div>
+      <div className="flex flex-row flex-wrap justify-center gap-10">
+        {technologies.map((technology) => (
+          <div className="w-28 h-28" key={technology.name}>
+            <BallCanvas icons={[technology.icon]} />
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -6,7 +6,6 @@ import CanvasLoader from "../Loader";
 
 const Earth = () => {
   const earth = useGLTF("/planet/scene.gltf");
-  console.log(earth)
 
   return (
     <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
