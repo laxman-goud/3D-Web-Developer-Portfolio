3a64accb22a561cae2478658a753b97f1a7dd1cf
laxman-goud <codewithlaxman92@gmail.com>
2025-09-06 12:57:00 +0530

    docs(app): add documentation and comments to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,51 @@
+/**
+ * App.jsx
+ * ---------------------------------------
+ * Root component of the portfolio application.
+ * Sets up routing with BrowserRouter and composes
+ * all major sections of the site.
+ *
+ * Sections included:
+ * - Navbar & Hero (header)
+ * - About
+ * - Tech (skills/technologies)
+ * - Works (projects showcase)
+ * - Contact + StarsCanvas (footer with 3D background)
+ *
+ * Note:
+ * - Experience & Feedbacks are present but currently commented out.
+ */
+
 import { BrowserRouter } from "react-router-dom";
-import {About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import {
+  About,
+  Contact,
+  Experience,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
 
 const App = () => {
   return (
     <BrowserRouter>
       <div className="relative z-0 bg-primary">
+        {/* Header section with navbar & hero background */}
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
         </div>
+
+        {/* Main content sections */}
         <About />
-        {/* <Experience /> */}
+        {/* <Experience />  Uncomment to show work experience */}
         <Tech />
         <Works />
-        {/* <Feedbacks /> */}
+        {/* <Feedbacks /> Uncomment when ready */}
+
+        {/* Footer with contact form & animated star background */}
         <div className="relative z-0">
           <Contact />
           <StarsCanvas />
@@ -21,6 +53,6 @@ const App = () => {
       </div>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
