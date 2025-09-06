c35d01a5a66931c4ff09a2a60be5cd7a3998c128
laxman-goud <codewithlaxman92@gmail.com>
2025-09-06 13:10:13 +0530

    docs: add documentation to CanvasLoader component

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,23 +1,28 @@
-import { Html, useProgress } from "@react-three/drei";
+import { Html, useProgress } from '@react-three/drei';
 
+/**
+ * CanvasLoader Component
+ * Displays a loading indicator with progress percentage while 3D assets load.
+ */
 const CanvasLoader = () => {
-  const { progress } = useProgress();
+  const { progress } = useProgress(); // Tracks loading progress
+
   return (
     <Html
       as='div'
       center
       style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
       }}
     >
       <span className='canvas-loader'></span>
       <p
         style={{
           fontSize: 14,
-          color: "#F1F1F1",
+          color: '#F1F1F1',
           fontWeight: 800,
           marginTop: 40,
         }}
